99bfdce08215fba119c0ea7339b42128161f67df
RomanVM <astek14@mail.ru>
2026-08-08 18:40:53 +0300

    3_9 delete student

---
+++ src/main/java/com/rk/jpa_crud/RemoveEx.java
@@ -0,0 +1,31 @@
+package com.rk.jpa_crud;
+
+import com.rk.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class RemoveEx {
+    public static void main(String[] args) {
+        try (EntityManagerFactory factory = Persistence.createEntityManagerFactory("jpa-course");
+             EntityManager entityManager = factory.createEntityManager()) {
+
+            EntityTransaction transaction = entityManager.getTransaction();
+            Student student = null;
+
+            try {
+                transaction.begin();
+                student = entityManager.find(Student.class, 1);
+                entityManager.remove(student);
+
+                transaction.commit();
+            } catch (Exception e) {
+                if (transaction.isActive()) {
+                    transaction.rollback();
+                }
+            }
+            System.out.println(student);
+        }
+    }
+}
