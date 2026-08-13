9acce11f93170486fe3c0959afe1ed76a3f83c79
RomanVM <astek14@mail.ru>
2026-08-13 17:41:14 +0300

    8_3 session. add entity to table

--- src/main/java/com/rk/hibernate/HibernateEx.java
+++ src/main/java/com/rk/hibernate/HibernateEx.java
@@ -1,7 +1,39 @@
 package com.rk.hibernate;
 
+import com.rk.hibernate.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateEx {
     public static void main(String[] args) {
+        try (SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+             Session session = factory.getCurrentSession()) {
 
+            Transaction transaction = session.getTransaction();
+
+            try {
+//                Student student = new Student("Chanel", "King", 9.1);
+                Student student = new Student("Leo", "Farrell", 8.4);
+
+                transaction.begin();
+
+//                System.out.println(session.contains(student));
+                session.persist(student);
+//                System.out.println(session.contains(student));
+                System.out.println(student);
+
+                transaction.commit();
+            } catch (Exception e) {
+                if (transaction.isActive()) {
+                    transaction.rollback();
+                }
+                e.printStackTrace();
+            }
+        }
     }
 }
